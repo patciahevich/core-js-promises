2c5e7a3794edeaeba3db879ab54f28bae80c57ec
Volha Patciahevich <zaprudskaya@gmail.com>
2024-03-20 22:50:55 +0300

    feat: solve queuPromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -140,8 +140,14 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return promises.reduce((acc, promise) => {
+    return acc.then((result) => {
+      return promise.then((item) => {
+        return result + item;
+      });
+    });
+  }, Promise.resolve(''));
 }
 
 module.exports = {
